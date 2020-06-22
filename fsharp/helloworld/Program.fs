2b57d5a92117ea7d5398fe6eef4990192f9e3c36
uta8a <noumodulity@gmail.com>
2020-06-22 21:17:22 +0900

    hello F#

---
+++ fsharp/helloworld/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Scanf
+
+[<EntryPoint>]
+let main argv =
+  printfn "What is the ultimate answer?"
+
+  try
+    let ans = scanfn "%i"
+    if ans = 42 then
+      printfn "correct."
+    else
+      printfn "%i? no." ans
+  with
+    | _ -> printfn "you entered something other than a number."
+  0 // return an integer exit code
